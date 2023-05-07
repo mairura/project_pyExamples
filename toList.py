50be20dc1c11235ccc4264c69b0b83fadf1a36eb
mairura <mairurated@gmail.com>
2023-05-07 23:45:34 +0300

    convert string to array

--- toList.py
+++ toList.py
@@ -1,5 +1,29 @@
+import re
+
+
 def convert_string_to_list(string):
-	return string.split(',')
+    return string.split(',')
+
 
 string = "apple,banana,cherry,date"
+print(convert_string_to_list(string))
+
+# Case 2
+
+
+def convert_string_to_list(string):
+    return [x.strip() for x in string.split(',')]
+
+
+string = "apple,banana,cherry,date"
+print(convert_string_to_list(string))
+
+# Case 3
+
+
+def convert_string_to_list(string):
+    return re.split(r'\s*,\s*', string)
+
+
+string = "apple, banana, cherry, date"
 print(convert_string_to_list(string))
