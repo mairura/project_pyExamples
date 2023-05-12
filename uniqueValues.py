1cebcf9f5a38ee3a451fe3344ef09bec726936c8
mairura <mairurated@gmail.com>
2023-05-12 22:33:00 +0300

    Print occurence of a value

---
+++ uniqueValues.py
@@ -0,0 +1,12 @@
+def count_occurrences(my_list):
+    counts = {}
+    for item in my_list:
+        if item in counts:
+            counts[item] += 1
+        else:
+            counts[item] = 1
+    return counts
+
+
+my_list = [4, 3, 2, 1, 3, 1, 2, 3, 1, 4, 4, 2, 1, 4]
+print(count_occurrences(my_list))
