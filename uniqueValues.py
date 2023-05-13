53e51abce02661691b61823e708cd0127bfcff16
mairura <mairurated@gmail.com>
2023-05-13 23:00:52 +0300

    Identify unique values using for loop and collections mode

--- uniqueValues.py
+++ uniqueValues.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def count_occurrences(my_list):
     counts = {}
     for item in my_list:
@@ -6,6 +9,16 @@ def count_occurrences(my_list):
         else:
             counts[item] = 1
     return counts
+
+
+my_list = [4, 3, 2, 1, 3, 1, 2, 3, 1, 4, 4, 2, 1, 4]
+print(count_occurrences(my_list))
+
+# Using collections mode
+
+
+def count_occurrences(my_list):
+    return Counter(my_list)
 
 
 my_list = [4, 3, 2, 1, 3, 1, 2, 3, 1, 4, 4, 2, 1, 4]
